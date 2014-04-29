5fd384871d49fa25c358f9f65ad08efdafb58772
Campbell Allen <campbell.allen@gmail.com>
2014-04-29 17:53:25 +0100

    add query for priority unseen subjects

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ActiveRecord::Base
   SUBJET_SELECT_WINDOW_SIZE = 100
 
   def random_unseen_subjects(num_to_select=10)
-    query = %{
-      SELECT DISTINCT PS.id
-      FROM project_subjects PS
-      WHERE PS.active = 't'
-      AND (NOT EXISTS (SELECT subject_id FROM user_seen_subjects USS where user_id = #{self.id} AND PS.id = USS.subject_id))
-      LIMIT (#{SUBJET_SELECT_WINDOW_SIZE})
-    }.gsub!("\n", "")
+    query = unseen_subject_query_base + " LIMIT #{SUBJET_SELECT_WINDOW_SIZE}"
     ActiveRecord::Base.connection.select_values(query).sample(num_to_select)
+  end
+
+  def ordered_unseen_subjects(num_to_select=10)
+    query = unseen_subject_query_base + " ORDER BY priority LIMIT #{num_to_select}"
+    ActiveRecord::Base.connection.select_values(query)
   end
 
   #same query but maybe slightly different query paths - see query plans in explain
@@ -19,4 +18,13 @@ class User < ActiveRecord::Base
     query = "id NOT IN (SELECT subject_id FROM user_seen_subjects where user_id = #{power_user_id})"
     ProjectSubject.where(active: true).where(sub_query).limit(100)
   end
+
+  private
+
+    def unseen_subject_query_base
+      @base_query_string ||=  "SELECT PS.id " +
+                              "FROM project_subjects PS " +
+                              "WHERE PS.active = 't' " +
+                              "AND (NOT EXISTS (SELECT subject_id FROM user_seen_subjects USS where user_id = #{self.id} AND PS.id = USS.subject_id))"
+    end
 end
--- lib/queries.rb
+++ lib/queries.rb
@@ -3,6 +3,7 @@ id_times = {}
 User.all.each do |user|
   id_times[user.id] = Benchmark.measure {
     user.random_unseen_subjects
+    # user.ordered_unseen_subjects
   }.real
 end
 sorted_id_times = id_times.sort_by{ |k,v| v }
@@ -34,9 +35,9 @@ min = sorted.min
 max = sorted.max
 len = sorted_id_times.length
 ave = sorted.inject(&:+) / len
-median = len % 2 == 1 ? sorted[len/2] : (sorted[len/2 - 1] + sorted[len/2]).to_f / 2
+median = len % 2 == 1 ? sorted[len/2] : (sorted[len/2 - 1] + sorted[len/2]).to_f / counts
 
-counts = {}
+2 = {}
 [ "1", "10", "20", "30", "45" ].each do |power_user_id|
   user = User.find(power_user_id)
   counts[user.id] = UserSeenSubject.where(user_id: user.id).count
@@ -45,7 +46,6 @@ counts.sort_by { |k,v| v }
 
 ### Query plans ###
 power_user_id = 1
-
 raw_query = %{ SELECT DISTINCT PS.id
   FROM project_subjects PS
   WHERE PS.active = 't'
@@ -57,8 +57,19 @@ print ActiveRecord::Base.connection.explain(raw_query)
 sub_query = "id NOT IN (SELECT subject_id FROM user_seen_subjects where user_id = #{power_user_id})"
 ProjectSubject.where(active: true).where(sub_query).limit(100).explain
 
+### PRIORITY QUERIES ####
+priority_query = %{ SELECT PS.id
+  FROM project_subjects PS
+  WHERE PS.active = 't'
+  AND (NOT EXISTS (SELECT subject_id FROM user_seen_subjects USS where user_id = #{power_user_id} AND PS.id = USS.subject_id))
+  ORDER BY priority
+  LIMIT 10
+}.gsub!("\n", "")
+print ActiveRecord::Base.connection.explain(priority_query)
+
 ## QUERY TIMES ###
 ActiveRecord::Base.connection.select_values(raw_query)
+ActiveRecord::Base.connection.select_values(priority_query)
 ProjectSubject.where(active: true).where(sub_query).limit(100)
 
 
