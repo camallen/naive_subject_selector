0c622c489eb3345c3a8b489f2f3a636d5fc63e34
Campbell Allen <campbell.allen@gmail.com>
2014-04-30 10:13:37 +0100

    do not include User.find query time in classification benchmark

--- app/models/user_seen_subject.rb
+++ app/models/user_seen_subject.rb
@@ -1,6 +1,6 @@
 class UserSeenSubject < ActiveRecord::Base
 
 	def self.reset_user_seen_subjects
-	  ActiveRecord::Base.connection.execute("TRUNCATE user_seen_subjects")
+	  ActiveRecord::Base.connection.execute("TRUNCATE user_seen_subjects; ALTER SEQUENCE user_seen_subjects_id_seq RESTART WITH 1;")
 	end
 end
--- lib/classification_simulation.rb
+++ lib/classification_simulation.rb
@@ -18,18 +18,21 @@ class ClassificationSimulation
     3000.times do
       @active_user_set.each do |user_id|
         #under no load my workstation can do ~ 100 in 0.88 secs, so ~0.0088 sec / insert
-        #@note: insert is a lot slower than the update for arrays and with more contention for the DB this will only bump!
+        ### ??? WHY IS THIS??? ####
+        #under no load my laptop      can do ~ 100 in 0.18 secs, so ~0.0018 sec / insert
+        #@note: with more process / request contention for the DB this number degrades.
+
         #attempt to rate limit the input of classifications (using the above then these limits will roughly work)
+        # (0.0018) = 555/s 33300/min -> As fast as the laptop can go
         # (0.0025) = 400/s 24000/min
-        # -> no sleep just run as fast as it can
-        # (0.01)   = 100/s 6000/min 
+        # (0.01)   = 100/s 6000/min
         # (0.02)   = 50/s  3000/min
         # (0.05)   = 20/s  1200/min
         # (0.067)  = 15/s  900/min
         # (0.1)    = 10/s  600/min
         # (0.17)   = 6/s   360/min
         # (0.67)   = 1.5/s 90/min
-        sleep(0.01)
+        # sleep(0.01)
         classify_subject(user_id)
         #TODO: what about retiring a subject
         # post benchmark?
@@ -42,12 +45,23 @@ class ClassificationSimulation
   private
 
     def classify_subject(user_id)
-      user = User.find(user_id)
-      subject_id = user.random_unseen_subjects.first
-      #insert_sql = "INSERT INTO user_seen_subjects (user_id, subject_id) VALUES (#{user_id}, #{subject_id});"
-      #ActiveRecord::Base.connection.execute(insert_sql)
-      UserSeenSubject.new(user_id: user_id, subject_id: subject_id).save(validate: false)
+      subject_id = select_random_unseen_subject(user_id)
+      insert_sql = "INSERT INTO user_seen_subjects (user_id, subject_id, created_at, updated_at) VALUES (#{user_id}, #{subject_id}, now(), now());"
+      ActiveRecord::Base.connection.execute(insert_sql)
+      # @note: create / new + save without validation slower than raw insert
+      # UserSeenSubject.create(user_id: user_id, subject_id: subject_id)
+      # UserSeenSubject.new(user_id: user_id, subject_id: subject_id).save(validate: false)
       @number_classified += 1
+    end
+
+    #Avoid slowing down insert rate of classifications by including the User.find query time
+    def select_random_unseen_subject(user_id)
+      query = "SELECT PS.id " +
+              "FROM project_subjects PS " +
+              "WHERE PS.active = 't' " +
+              "AND (NOT EXISTS (SELECT subject_id FROM user_seen_subjects USS where user_id = #{user_id} AND PS.id = USS.subject_id)) " +
+              "LIMIT 100"
+      ActiveRecord::Base.connection.select_values(query).sample(10).first
     end
 
     def median_query_time
